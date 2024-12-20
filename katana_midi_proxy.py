723fd92a3429cf8f6cc38ccbd47f63fc9d453baa
= <=>
2024-12-19 16:15:34 -0800

    Bugfixes and tweaks from initial testing

--- katana_midi_proxy.py
+++ katana_midi_proxy.py
@@ -1,10 +1,11 @@
 """
-Mididings script translating from a couple of ActitioN MIDI controller
-footpedals to control of a Katana MkII Head amplifier.
+Mididings script that sits between a Behringer FCB1010 MIDI controller running
+Wino2 firmware and a Katana MkII Head amplifier. This allows much more complete
+control over the Katana than most other foot pedal solutions.
 
-MIDI out from the controllers should be routed into `katana_proxy` input, and
-`katana_proxy` outputs routed to the Katana and the ActitioN programmable
-controller.
+The proxy exposes `out_to_katana` and `out_to_controller` outputs to connect to
+the device's MIDI inputs. MIDI out from both the controller and the amp should
+ be routed to the proxy's single input port.
 """
 import inspect
 import logging
@@ -16,7 +17,7 @@ from mididings import *
 from mididings import engine
 from mididings import event
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 # logging.basicConfig(level=logging.DEBUG)
 
 config(
@@ -73,7 +74,7 @@ sysex_cmds = {
 		1: "12 60 00 06 3b 01",
 		2: "12 60 00 06 3b 02",
 		},
-	"delay1_color": {
+	"delay_color": {
 		0: "12 60 00 06 3c 00",
 		1: "12 60 00 06 3c 01",
 		2: "12 60 00 06 3c 02",
@@ -125,8 +126,8 @@ sysex_cmds = {
 	}
 
 sysex_cmds_subs = {
-	"select_amp": "12 00 01 00 00 00 {}",
-	"delay_tap":  "12 60 00 05 {} {} {}",
+	"select_amp": "12 00 01 00 00 00 {:02x}",
+	"delay_tap":  "12 60 00 05 {:02x} {:02x} {:02x}",
 	}
 
 query_cmds = [
@@ -192,7 +193,7 @@ def format_sysex_cmd(cmd_hex):
 	cmd_ints = [int(v, 16) for v in cmd]
 	checksum = '{:02x}'.format(get_checksum(cmd_ints))
 	formatted = " ".join((PREFIX, send_or_rcv, " ".join(cmd), checksum, 'f7'))
-	logging.debug("formatted sysex_cmd: {}", formatted)
+	logging.debug("formatted sysex_cmd: %s", formatted)
 	return formatted
 
 
@@ -209,12 +210,12 @@ def send_query_cmds(port):
 
 
 def log_fn_call(outer_fn):
-	def fn(self, *args, **kwargs):
-		logging.info("ENTER {}", outer_fn.__name__)
-		logging.info("args: {}", args)
+	def fn(*args, **kwargs):
+		logging.info("ENTER %s", outer_fn.__name__)
+		logging.info("args: %s", args)
 		if kwargs:
-			logging.info("kwargs: {}", kwargs)
-		return outer_fn(self, *args, **kwargs)
+			logging.info("kwargs: %s", kwargs)
+		return outer_fn(*args, **kwargs)
 	return fn
 
 
@@ -254,7 +255,7 @@ class Amp(object):
 		"""
 		sysex_cmd = sysex_cmds[cmd_name][key]
 		caller_name = inspect.stack()[1][3]
-		logging.debug("fn name: {}; ev.port: {}; {}", caller_name, port,
+		logging.debug("fn name: %s; port: %s; %s", caller_name, self.port,
 					  sysex_cmd)
 		return event.SysExEvent(self.port, sysex_cmd)
 
@@ -265,17 +266,17 @@ class Amp(object):
 		be encoded into hex, and for the SysEx checksum to be calculated.
 		"""
 		fmt_args = ['{:02x}'.format(arg) for arg in args]
-		sysex_cmd = sysex_cmds[cmd_name].format(*args)
+		sysex_cmd = sysex_cmds_subs[cmd_name].format(*args)
 		sysex_cmd = format_sysex_cmd(sysex_cmd)
 		caller_name = inspect.stack()[1][3]
-		logging.debug("fn name: {}; ev.port: {}; {}", caller_name, port,
+		logging.debug("fn name: %s; port: %s; %s", caller_name, self.port,
 					  sysex_cmd)
 		return event.SysExEvent(self.port, sysex_cmd)
 
 	def get_effect(self, name):
 		effect = self.effects.get(name)
 		if effect is None:
-			logging.error("Unknown effect: {}", name)
+			logging.error("Unknown effect: %s", name)
 		return effect
 
 	@log_fn_call
@@ -306,7 +307,7 @@ class Amp(object):
 		self.bank = bank
 		# adjust patch to match the new bank
 		self.patch = self.patch + 4 if bank else self.patch - 4
-		return self.run_cmd_subs("select_amp", patch)
+		return self.run_cmd_subs("select_amp", self.patch)
 
 	@log_fn_call
 	def next_effect_color(self, ev, name):
@@ -316,13 +317,12 @@ class Amp(object):
 		effect = self.get_effect(name)
 		if effect is None:
 			return
-		if not effect.has_color():
-			logging.error("Effect has no color: {}", name)
+		if not effect.has_color:
+			logging.error("Effect has no color: %s", name)
 			return
 		# increment color value, looping to 0 after 2
-		color = effect.color
-		effect.color = 0 if color >= 2 else color + 1
-		return self.run_cmd(name+"_color", color)
+		effect.color = 0 if effect.color >= 2 else effect.color + 1
+		return self.run_cmd(name+"_color", effect.color)
 
 	@log_fn_call
 	def delay_tap(self, ev, name):
@@ -334,18 +334,20 @@ class Amp(object):
 		if effect is None:
 			return
 		if not effect.is_delay:
-			logging.error("Effect is not a delay: {}", name)
+			logging.error("Effect is not a delay: %s", name)
 			return
 		# use whole milliseconds
 		now = int(time.time() * 1000)
 		# always store the new tap time
-		last_tap = self.last_tap
-		self.last_tap = now
+		last_tap = effect.last_tap
+		effect.last_tap = now
 		interval = now - last_tap
 		# if first tap or > 2s since last tap, do nothing else
 		if interval > 2000:
 			return
 
+		# clear last tap so a third tap starts new instead of interfering
+		effect.last_tap = 0
 		# get 11 digit binary representation
 		interval_bin = "{:011b}".format(interval)
 		# first four digits make the first value, the rest the second
@@ -354,7 +356,7 @@ class Amp(object):
 		return self.run_cmd_subs("delay_tap", effect.tap_opcode, i_sub1, i_sub2)
 
 	@log_fn_call
-	def select_preset(ev):
+	def select_preset(self, ev):
 		"""
 		We accept program change (PC) values 2-5. (The controller sends 1-4,
 		which the Katana recognizes, but since `data_offset` is 1 so we can
@@ -362,7 +364,7 @@ class Amp(object):
 		"""
 		patch = ev.program - 1
 		if not 1 <= patch <= 4:
-			logging.error("Invalid patch value: {}", patch)
+			logging.error("Invalid patch value: %d", patch)
 			return
 		if self.bank > 0:
 			# upper bank is 5-8
@@ -394,7 +396,7 @@ class Controller(object):
 		amp_bank_pedal = LED_map.get("amp_bank")
 		if amp_bank_pedal is None:
 			return
-		logging.info("Sending PC {} to controller - amp_bank", amp_bank_pedal)
+		logging.info("Sending PC %d to controller - amp_bank", amp_bank_pedal)
 		output_event(event.ProgramEvent(self.port, self.channel, amp_bank_pedal))
 
 	def update_bank(self, ev):
@@ -407,17 +409,17 @@ class Controller(object):
 		effect_pedal = LED_map.get(effect.name)
 		if effect_pedal is None:
 			return
-		logging.info("Sending PC {} to controller - {}", effect_pedal,
+		logging.info("Sending PC %d to controller - %s", effect_pedal,
 					 effect.name)
 		output_event(event.ProgramEvent(self.port, self.channel, effect_pedal))
 
 	def set_preset(self, preset):
-		LED_map = ctrl_LED_MAPS[self.bank]
+		LED_map = ctrl_LED_maps[self.bank]
 		preset_pedal = LED_map.get("presets")
 		if preset_pedal is None:
 			return
 		preset_pedal = preset_pedal + preset - 1
-		logging.info("Sending PC {} to controller - preset {}", preset_pedal,
+		logging.info("Sending PC %d to controller - preset %d", preset_pedal,
 					 preset)
 		output_event(event.ProgramEvent(self.port, self.channel, preset_pedal))
 
@@ -450,7 +452,7 @@ class QueryProcessor(object):
 		state.
 		"""
 		hex_bytes = ' '.join('{:02x}'.format(x) for x in ev.sysex)
-		logging.debug("SysEx event rec'd: {}", hex_bytes)
+		logging.debug("SysEx event rec'd: %s", hex_bytes)
 
 		# extract the starting address and an unknown number of data bytes
 		start_address = hex_bytes[24:35]
@@ -466,20 +468,37 @@ class QueryProcessor(object):
 				address = start_address
 				last_addr_byte = address[-2:]
 			else:
-				address = increment_address
+				address = self.increment_address(address)
 
 			# see if this byte's address maps to a known setting
 			setting_name = addresses.get(address)
 			if not setting_name:
 				# no match, ignore it
 				continue
+			logging.info("Match: %s", setting_name)
 
-			logging.info("Match: ()", setting_name)
+			if setting_name == "patch_selected":
+				patch = int(data_bytes[-1], 16)
+				if amp.patch == patch:
+					continue
+				logging.info("patch: %d", patch)
+				# update amp state
+				amp.patch = patch
+				# update controller `amp_bank` state
+				amp_bank_of_patch = int(patch > 4)
+				# `toggle_amp_bank` checks to see if they match so we don't
+				# need to
+				ctrl.toggle_amp_bank(amp_bank_of_patch)
+				ctrl_preset = patch - 4	if amp_bank_of_patch else patch
+				ctrl.set_preset(ctrl_preset)
+				continue
+
 			data_byte = int(data_byte, 16)
 			effect_name, action = setting_name.rsplit("_", 1)
 			effect = amp.get_effect(effect_name)
 			if action == "color":
-				# controller doesn't support colors so we just update state
+				# controller doesn't support colors so we just update
+				# amp/effect state
 				effect.color = data_byte
 				continue
 
@@ -487,20 +506,12 @@ class QueryProcessor(object):
 				# python equates 0 & 1 with True & False
 				if effect.on == data_byte:
 					continue
+				# update amp/effect state
 				effect.on = bool(data_byte)
+				# update controller state
 				ctrl.toggle_effect(effect)
-				continue
 
-			if setting_name == "patch_selected":
-				patch = int(data_bytes[-1], 16)
-				if amp.patch == patch:
-					continue
-				logging.info("patch: {}".format(patch))
-				amp_bank_of_patch = int(patch > 4)
-				# `ctrl.toggle_amp_bank` checks to see if they already match
-				ctrl.toggle_amp_bank(amp_bank_of_patch)
-				ctrl_preset = patch - 4	if amp_bank_of_patch else patch
-				ctrl.set_preset(ctrl_preset)
+qproc = QueryProcessor()
 
 
 def init():
@@ -536,7 +547,7 @@ run(
 			98: CtrlValueFilter(127) >> Process(amp.next_effect_color, "fx"),
 			99: CtrlValueFilter(127) >> Process(amp.next_effect_color,
 												 "reverb"),
-			100: CtrlValueFilter(127) >> Process(amp.delay_tap, "delay1"),
+			100: CtrlValueFilter(127) >> Process(amp.delay_tap, "delay"),
 			101: CtrlValueFilter(127) >> Process(amp.delay_tap, "delay2"),
 
 			102: Process(amp.toggle_effect, "solo"),
@@ -545,12 +556,12 @@ run(
 												 "global_eq"),
 
 			125: Process(ctrl.update_amp_bank),
-			126: Process(ctrl.update_bank),
+			126: Call(ctrl.update_bank),
 			None: Pass(),
 			}),
 		ProgramFilter([2, 3, 4, 5]) >> Process(amp.select_preset),
 		# SysEx messages are query results back from the amp
-		SysExFilter(manufacturer=0x41) >> Call(process_query_result),
-		SysExFilter("\xf0\x7e") >> Call(process_query_result),
+		SysExFilter(manufacturer=0x41) >> Call(qproc.process_query_result),
+		SysExFilter("\xf0\x7e") >> Call(qproc.process_query_result),
 	]
 	)
